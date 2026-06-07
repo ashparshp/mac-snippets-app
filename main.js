aa145002b9139c2e10d2e0a755949e1fa35e5f65
Ashparshp <ashparsh.connects@gmail.com>
2026-06-07 23:31:24 +0530

    refactor: remove electron-is-dev dependency, update build scripts, and ignore release artifacts

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import { app, BrowserWindow, globalShortcut, Menu, ipcMain } from 'electron';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import isDev from 'electron-is-dev';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -22,6 +21,8 @@ function createWindow() {
       contextIsolation: false,
     },
   });
+
+  const isDev = !app.isPackaged;
 
   if (isDev) {
     mainWindow.loadURL('http://localhost:5173');
